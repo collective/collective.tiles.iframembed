374c2370c27868f65d269e4e490f8b4bf66a4e8e
daniele-andreotti <daniele.andreotti@redturtle.it>
2018-02-05 11:53:58 +0100

    Added check on application url

--- src/collective/tiles/iframembed/tile.py
+++ src/collective/tiles/iframembed/tile.py
@@ -3,6 +3,7 @@ from plone import tiles
 from plone import api
 from plone.app.uuid.utils import uuidToObject
 from collective.tiles.iframembed.interfaces import IIFrameEmbedTilesSettings
+from urlparse import urlparse
 
 
 class FrameEmbedTile(tiles.PersistentTile):
@@ -22,3 +23,11 @@ class FrameEmbedTile(tiles.PersistentTile):
 
         api.portal.show_message("L'url indicato non e' valido per i domini ammessi", self.request, 'error')
         return False
+
+    def eval_query_string(self):
+        o = urlparse(self.request.HTTP_REFERER)
+        query_string = o.query
+        query_string = query_string.replace('?', "/", 1)
+        query_string = query_string.replace('&', "?", 1)
+        result = '%s/%s' % (self.data.get('url_to_embed'), query_string)
+        return result
