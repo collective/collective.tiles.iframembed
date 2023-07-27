ebd702c2e3c4f0d6eff6203baec0227005d15792
Daniele Andreotti <daniele.andreotti@redturtle.it>
2023-07-27 13:26:28 +0200

    handle SearchableText in querystring

--- src/collective/tiles/iframembed/tile.py
+++ src/collective/tiles/iframembed/tile.py
@@ -1,4 +1,4 @@
-#-*- encoding:utf-8 *-
+# -*- encoding:utf-8 *-
 from plone import tiles
 from plone import api
 from plone.app.uuid.utils import uuidToObject
@@ -7,30 +7,34 @@ from urlparse import urlparse
 
 
 class FrameEmbedTile(tiles.PersistentTile):
-
     def iframe_validation(self):
-        url_to_embed = self.data.get('url_to_embed')
+        url_to_embed = self.data.get("url_to_embed")
         if not url_to_embed:
             return False
 
         valid_domains = api.portal.get_registry_record(
-            'available_domains',
-            interface=IIFrameEmbedTilesSettings)
+            "available_domains", interface=IIFrameEmbedTilesSettings
+        )
 
         for domain in valid_domains:
             if url_to_embed.find(domain) != -1:
                 return True
 
-        api.portal.show_message("L'url indicato non e' valido per i domini ammessi", self.request, 'error')
+        api.portal.show_message(
+            "L'url indicato non e' valido per i domini ammessi", self.request, "error"
+        )
         return False
 
     def eval_query_string(self):
         o = urlparse(self.request.HTTP_REFERER)
         query_string = o.query
-        if query_string.startswith('_authenticator'):
-            return (self.data.get('url_to_embed')).rstrip('/')
-            
-        query_string = query_string.replace('?', "/", 1)
-        query_string = query_string.replace('&', "?", 1)
-        result = '%s/%s' % (self.data.get('url_to_embed'), query_string)
-        return result.rstrip('/')
+        if (
+            query_string.startswith("_authenticator")
+            or "SearchableText" in query_string
+        ):
+            return (self.data.get("url_to_embed")).rstrip("/")
+
+        query_string = query_string.replace("?", "/", 1)
+        query_string = query_string.replace("&", "?", 1)
+        result = "%s/%s" % (self.data.get("url_to_embed"), query_string)
+        return result.rstrip("/")
